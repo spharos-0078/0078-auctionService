5c7226167fd84e3e1f3027d9bcd35f8509c2f5c9
JungDongSeop <ds123911@gmail.com>
2025-06-04 13:57:02 +0900

    [auction] feat: microbatch 위한 queue 작성

--- src/main/java/com/pieceofcake/auction_service/bid/application/BidServiceImpl.java
+++ src/main/java/com/pieceofcake/auction_service/bid/application/BidServiceImpl.java
@@ -1,8 +1,10 @@
 package com.pieceofcake.auction_service.bid.application;
 
+import com.pieceofcake.auction_service.bid.application.batch.BidQueueService;
 import com.pieceofcake.auction_service.bid.dto.in.CreateBidRequestDto;
 import com.pieceofcake.auction_service.bid.dto.in.ReadBidRequestDto;
 import com.pieceofcake.auction_service.bid.dto.out.ReadBidResponseDto;
+import com.pieceofcake.auction_service.bid.entity.Bid;
 import com.pieceofcake.auction_service.bid.infrastructure.BidRepository;
 import com.pieceofcake.auction_service.common.entity.BaseResponseStatus;
 import com.pieceofcake.auction_service.common.exception.BaseException;
@@ -14,11 +16,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BidServiceImpl implements BidService{
     private final BidRepository bidRepository;
+    private final BidQueueService bidQueueService;
 
     @Override
     @Transactional
     public void createBid(CreateBidRequestDto createBidRequestDto) {
-        bidRepository.save(createBidRequestDto.toEntity());
+        Bid bid = createBidRequestDto.toEntity();
+        // bid 저장
+        bidRepository.save(bid);
+        // 현재 1초 배치 주기에 처리되도록 큐에 추가
+        bidQueueService.addBid(bid);
     }
 
     @Override
