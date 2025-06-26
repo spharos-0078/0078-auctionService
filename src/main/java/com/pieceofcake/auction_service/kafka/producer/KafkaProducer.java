704c76e251a7b8173201522917fbca1448355e51
JungDongSeop <109326290+JungDongSeop@users.noreply.github.com>
2025-06-26 10:51:27 +0900

    fix: transaction 고려
    * fix: transaction 생각해서 feign client 2개 보낼 때 순서 고려
    
    * feat: auction/vote create 시 piece에 kafak 통신 전달 (긴급)

--- src/main/java/com/pieceofcake/auction_service/kafka/producer/KafkaProducer.java
+++ src/main/java/com/pieceofcake/auction_service/kafka/producer/KafkaProducer.java
@@ -9,8 +9,16 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    public void sendVoteStartEvent(String productUuid) {
+        kafkaTemplate.send("vote-start", productUuid); // 단순히 productUuid만 전송
+    }
+
     public void sendVoteCloseEvent(String productUuid) {
         kafkaTemplate.send("vote-close", productUuid); // 단순히 voteUuid만 전송
+    }
+
+    public void sendAuctionStartEvent(String productUuid) {
+        kafkaTemplate.send("auction-start", productUuid);
     }
 
     public void sendAuctionCloseEvent(String productUuid) {
--- src/main/java/com/pieceofcake/auction_service/vote/application/VoteServiceImpl.java
+++ src/main/java/com/pieceofcake/auction_service/vote/application/VoteServiceImpl.java
@@ -2,6 +2,7 @@ package com.pieceofcake.auction_service.vote.application;
 
 import com.pieceofcake.auction_service.common.entity.BaseResponseStatus;
 import com.pieceofcake.auction_service.common.exception.BaseException;
+import com.pieceofcake.auction_service.kafka.producer.KafkaProducer;
 import com.pieceofcake.auction_service.vote.dto.in.CreateVoteRequestDto;
 import com.pieceofcake.auction_service.vote.dto.in.ReadVoteDetailRequestDto;
 import com.pieceofcake.auction_service.vote.dto.in.ReadVoteRequestdto;
@@ -11,19 +12,28 @@ import com.pieceofcake.auction_service.vote.infrastructure.VoteRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @RequiredArgsConstructor
 public class VoteServiceImpl implements VoteService {
 
     private final VoteRepository voteRepository;
-
+    private final KafkaProducer kafkaProducer;
     @Override
     @Transactional
     public void createVote(CreateVoteRequestDto createVoteRequestDto) {
         Vote vote = createVoteRequestDto.toEntity();
 
         voteRepository.save(vote);
+        // 투표 시작 이벤트 발행
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                kafkaProducer.sendAuctionStartEvent(vote.getProductUuid());
+            }
+        });
     }
 
     @Override
